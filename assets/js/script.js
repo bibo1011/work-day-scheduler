cb5350f3f7dcaf5ab7da708bba05060c4d7ae435
Blue Abad <bibo1011@gmail.com>
2020-08-22 21:55:18 -0700

    fix textarea alignment

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,51 @@ console.log(currentDay);
 
 // });
 
-$("button").on("click", function(){
-    var event = document.querySelector("textarea");
-    localStorage.setItem("event", event.value);
-    console.log(event.value);
+$("#btn9").on("click", function(){
+    var event9 = document.getElementById("9am");
+    localStorage.setItem("event9am", event9.value);
+    console.log(event9.value);
 })
-
-// $("<button>").on("click",function(){
-//     $()
-// })
+    $("#btn10").on("click", function(){ 
+    var event10 = document.getElementById("10am");
+    localStorage.setItem("event10am", event10.value);
+    console.log(event10.value);
+})    
+$("#btn11").on("click", function(){    
+    var event11 = document.getElementById("11am");
+    localStorage.setItem("event11am", event11.value);
+    console.log(event11.value);  
+})
+$("#btn12").on("click", function(){    
+    var event12 = document.getElementById("12am");
+    localStorage.setItem("event12am", event12.value);
+    console.log(event12.value);
+})
+$("#btn1").on("click", function(){    
+    var event1 = document.getElementById("1pm");
+    localStorage.setItem("event1pm", event1.value);
+    console.log(event1.value);  
+})
+$("#btn2").on("click", function(){    
+    var event2 = document.getElementById("2pm");
+    localStorage.setItem("event2pm", event2.value);
+    console.log(event2.value);  
+})
+$("#btn3").on("click", function(){    
+    var event3 = document.getElementById("3pm");
+    localStorage.setItem("event3pm", event3.value);
+    console.log(event3.value);  
+})
+$("#btn4").on("click", function(){    
+    var event4 = document.getElementById("4pm");
+    localStorage.setItem("event4pm", event4.value);
+    console.log(event4.value);  
+})
+$("#btn5").on("click", function(){    
+    var event5 = document.getElementById("5pm");
+    localStorage.setItem("event5pm", event5.value);
+    console.log(event5.value);  
+})
 
 // var events = {};
 // var saveEvents = function() {
