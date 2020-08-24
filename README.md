2f13ee2e4c6aebec620fb8ba81e71472a54ac0f6
Blue Abad <bibo1011@gmail.com>
2020-08-23 18:21:25 -0700

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # work-day-scheduler
+This challenge requires to create a simple calendar application that allows a user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+## Built with 
+*HTML
+*CSS
+*Javascript
+
+## Website:  https://bibo1011.github.io/work-day-scheduler/
+
+## Screenshot:
+
+![alt text] [screenshot]
+[screenshot]:  https://bibo1011.github.io/work-day-scheduler/assets/images/workday.png "work day scheduler"
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,14 @@ var currentDay= moment().format("dddd, MMMM Do");
 $("#currentDay").text(currentDay);
 console.log(currentDay);
 
-// $("<textarea>").click(function() {
-
-// });
+// adding event 
 
 $("#btn9").on("click", function(){
     var event9 = document.getElementById("9am");
     localStorage.setItem("event9am", event9.value);
     console.log(event9.value);
 })
-    $("#btn10").on("click", function(){ 
+$("#btn10").on("click", function(){ 
     var event10 = document.getElementById("10am");
     localStorage.setItem("event10am", event10.value);
     console.log(event10.value);
@@ -53,52 +51,12 @@ $("#btn5").on("click", function(){
     console.log(event5.value);  
 })
 
-// var events = {};
-// var saveEvents = function() {
-//     localStorage.setItem("events", JSON.stringify(events));
-//     $("<button>").on("click", "p", function() {
-//       var text = $(this)
-//         .text()
-//         .trim();
-//       console.log(text);
-//     })
+
+
+// var officeHours = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5apm"];
+// for (var i = 0; officeHours.length; i++){
+//     console.log(officeHours[i])
 // }
 
-// // create an event
-// var createTask = function(taskText, taskDate, taskList) {
-//     // create elements that make up a task item
-//     var taskLi = $("<li>").addClass("list-group-item");
-//     var taskSpan = $("<span>")
-//       .addClass("badge badge-primary badge-pill")
-//       .text(taskDate);
-//     var taskP = $("<p>")
-//       .addClass("m-1")
-//       .text(taskText);
 
-// $("time-block").on("blur", "textarea", function(){
-//     // get textarea's current value/text
-//     var text = $(this)
-//       .val()
-//       .trim();
 
-//     // get the parent ul's id attribute
-//     var status = $(this)
-//       .closest(".list-group")
-//       .attr("id")
-//       .replace("list-", "");
-
-//     // get the task's position in the list of other li elements
-//     var index = $(this)
-//       .closest(".list-group-item")
-//       .index();
-//     tasks[status][index].text = text;
-//     saveTasks();
-
-//     // recreate p element
-//     var taskP = $("<p>")
-//       .addClass("m-1")
-//       .text(text);
-
-//     // replace textarea with p element
-//     $(this).replaceWith(taskP);
-//   });
